ddb59c09019caade4efdc1eab2144ca403ffee80
nguepempoue <nguepempouejulio@gmail.com>
2023-07-10 17:18:37 +0200

    version de test avec le jar

--- src/main/java/org/sid/demoweb/DemoWebApplication.java
+++ src/main/java/org/sid/demoweb/DemoWebApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+//Version de déploiement avec jar
 @SpringBootApplication
 @RestController
 public class DemoWebApplication {
